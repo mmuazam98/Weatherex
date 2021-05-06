70b744a28a7e06d37e33fe3eac56836427db9e5b
Mohammad MuaZam <mm9084@srmist.edu.in>
2021-05-07 04:32:43 +0530

    Test 2.1.4

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,10 +1,10 @@
 let list = document.querySelectorAll(".list");
-for (let i = 0; i < list.length; i++) {
-  // list[i].onclick = () => {
-  //   let j = 0;
-  //   while (j < list.length) {
-  //     list[j++].className = "list";
-  //   }
-  //   list[i].className = "list active";
-  // };
-}
+// for (let i = 0; i < list.length; i++) {
+// list[i].onclick = () => {
+//   let j = 0;
+//   while (j < list.length) {
+//     list[j++].className = "list";
+//   }
+//   list[i].className = "list active";
+// };
+// }
